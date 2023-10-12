f868434e8cab17e6172b0d501257db93d1240b63
Bego Gonzalez <bego.gonzalez@outlook.com>
2023-10-12 11:42:43 +0200

    Age field added. Errors management improved.

--- PEC1_Ej2_1/script.js
+++ PEC1_Ej2_1/script.js
@@ -1,8 +1,10 @@
 const form = document.getElementById('form');
-const username = document.getElementById('username');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const password2 = document.getElementById('password2');
+const username = document.getElementById('usuario');
+const age = document.getElementById('edad');
+const email = document.getElementById('correo electrónico');
+const password = document.getElementById('contraseña');
+const password2 = document.getElementById('contraseña2');
+
 
 // Show input error message
 function showError(input, message) {
@@ -17,18 +19,11 @@ function showSuccess(input) {
     const formControl = input.parentElement;
     formControl.className = 'form-control success';
 }
-
-// Check email is valid
-function isValidEmail(email) {
-    const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-    return re.test(String(email).toLocaleLowerCase());
-}
-
 // Check required fields
 function checkRequired(inputArr) {
     inputArr.forEach(function (input) {
-        if(input.value.trim () === '') {
-            showError(input, `${getFieldName(input)} es obligatorio`)
+        if (input.value.trim() === '' || input.value.length == 0) {
+            showError(input, `El campo ${getFieldName(input)} es obligatorio`)
         } else {
             showSuccess(input);
         }
@@ -41,10 +36,55 @@ function getFieldName(input) {
     return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
+// Check age
+function checkAge(input){
+    console.log("la longitud es:" + input.value);
+    if (input.value >= 0 && input.value < 1000 && input.value.length>0){
+        showSuccess(input);
+    } else {
+        showError(input, `El campo ${getFieldName(input)} debe estar entre 0 y 1000`)
+    }
+}
+// Check email is valid
+function checkEmail(input) {
+    const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    if (re.test(input.value.trim())) {
+        showSuccess(input);
+    } else {
+        showError(input, `El campo ${getFieldName(input)} no es válido`);
+    }
+}
+
+// Check password validity
+function checkPasswordVal(input) {
+    const re = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[~!@#$%^&*()_+\-=\{\}\|\[\]\\:";'<>?,./])[^]{8,}$/i;
+    if (re.test(input.value)) {
+        showSuccess(input);
+    } else {
+        showError(input, `El campo ${getFieldName(input)} debe contener mayus, min, números y algún símbolo y una longitud mínima de 8 caracteres`)
+    }
+}
+
+// Check passwords match
+function checkPasswordMatch(input1, input2) {
+    if (input1.value !== input2.value) {
+        showError(input2, 'Las contraseñas no coinciden');
+    } else {
+        showSuccess(input2);
+    }
+}
+
+
 // Event listeners
 form.addEventListener('submit', function (e) {
     e.preventDefault();
+    
+    checkAge(age);
+    checkRequired([username, age, email, password, password2]);
+    checkEmail(email);
+    checkPasswordMatch(password, password2);
+    checkPasswordVal(password);
 
-    checkRequired([username, email, password, password2]);
+
 
 });
